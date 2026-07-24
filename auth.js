0fa454f75b1d0c66f2d33e8a867c938bb6dfa2a3
ShauMiNiuw <as-smn5@gmx.de>
2026-07-24 11:21:15 +0200

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,5 @@
+import { auth } from "./firebase.js";
+
+console.log("Firebase erfolgreich verbunden!");
+
+console.log(auth);
---
+++ firebase.js
@@ -0,0 +1,22 @@
+// Firebase SDK importieren
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+// Firebase-Konfiguration
+const firebaseConfig = {
+  apiKey: "AIzaSyAWIbmOQoheSs2Hq8Qyknf_SFrqmp2fjgo",
+  authDomain: "latein-vokabeltrainer-69fa9.firebaseapp.com",
+  projectId: "latein-vokabeltrainer-69fa9",
+  storageBucket: "latein-vokabeltrainer-69fa9.firebasestorage.app",
+  messagingSenderId: "226078015582",
+  appId: "1:226078015582:web:79d57b628292a7ea00f4cf",
+  measurementId: "G-31X1VWHKJB"
+};
+
+// Firebase starten
+const app = initializeApp(firebaseConfig);
+
+// Dienste exportieren
+export const auth = getAuth(app);
+export const db = getFirestore(app);
