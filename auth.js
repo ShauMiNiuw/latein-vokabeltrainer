c296f58530c70075899d702df9b64bcb87e8e0e7
ShauMiNiuw <as-smn5@gmx.de>
2026-07-24 11:31:33 +0200

    Add files via upload

--- auth.js
+++ auth.js
@@ -1,5 +1,47 @@
 import { auth } from "./firebase.js";
+import {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
-console.log("Firebase erfolgreich verbunden!");
+window.register = async function () {
 
-console.log(auth);
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    try {
+        await createUserWithEmailAndPassword(auth, email, password);
+
+        alert("Registrierung erfolgreich!");
+
+    } catch (error) {
+        alert(error.message);
+    }
+
+}
+
+window.login = async function () {
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    try {
+
+        await signInWithEmailAndPassword(auth, email, password);
+
+        alert("Login erfolgreich!");
+
+    } catch (error) {
+
+        alert(error.message);
+
+    }
+
+}
+
+window.logout = async function () {
+
+    await signOut(auth);
+
+}
